ee5326b2d63a550eebd813d808094a9577fe35dc
yangjianggui <1615031896@qq.com>
2021-11-02 09:17:36 +0800

    fix: 加一些注释

--- scripts/start.js
+++ scripts/start.js
@@ -1,17 +1,16 @@
 'use strict';
 
-// Do this as the first thing so that any code reading it knows the right env.
+// 设置环境变量
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
 
-// Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise rejections that are not handled will
-// terminate the Node.js process with a non-zero exit code.
+console.log(process.env.NODE_ENV)
+// 如果进程发生了未捕捉的异常，会触发uncaughtException事件，如果有报错，直接退出
 process.on('unhandledRejection', err => {
   throw err;
 });
 
-// Ensure environment variables are read.
+// 导入config文件夹下达env.js文件并执行
 require('../config/env');
 
 
