96ce8189e1ec3ab7cb77cde6b2c1d9687b1f37a4
yangjianggui <1615031896@qq.com>
2021-11-16 20:11:28 +0800

    fix: 加载load样式修改

--- src/views/system/userManage/index.jsx
+++ src/views/system/userManage/index.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
-import { Input } from 'antd'
+import React, { useLayoutEffect, useRef, useState } from 'react'
+import { Input, Table } from 'antd'
 import { QueryWrapper, FormItem, PageContent } from '@/components'
 
 const UserManage = () => {
+  const tableRef = useRef()
+  const [tableHeight, setTableHeight] = useState(0)
+
+  const columns = [
+    {
+      title: '序号',
+    },
+    {
+      title: '用户名',
+    },
+    {
+      title: '手机号',
+    },
+    {
+      title: '邮箱',
+    },
+    {
+      title: '地址',
+    },
+  ]
+
+  const dataSource = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+  ]
+
+  useLayoutEffect(() => {
+    const pageHeight = document.body.offsetHeight
+    const { top } = tableRef.current.getBoundingClientRect()
+    console.log(pageHeight, tableRef.current.getBoundingClientRect())
+    setTableHeight(pageHeight - top - 130)
+  }, [tableHeight])
+
   return (
     <PageContent>
       <QueryWrapper>
@@ -25,6 +84,19 @@ const UserManage = () => {
           <Input />
         </FormItem>
       </QueryWrapper>
+      <div ref={tableRef} style={{ marginTop: 10, backgroundColor: '#fff' }}>
+        <Table
+          columns={columns}
+          dataSource={dataSource}
+          pagination={{
+            pageSize: 20,
+          }}
+          scroll={{
+            x: 1000,
+            y: tableHeight,
+          }}
+        ></Table>
+      </div>
     </PageContent>
   )
 }
