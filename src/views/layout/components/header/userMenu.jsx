81a661c3c58dac2e4e8014aff4f1ea68df06e6c0
yangjianggui <1615031896@qq.com>
2021-11-20 21:49:12 +0800

    fix: 登录页编写，header用户信息编写

--- src/views/layout/components/header/userMenu.jsx
+++ src/views/layout/components/header/userMenu.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
-import { Avatar } from 'antd'
-import { UserOutlined } from '@ant-design/icons'
+import { useHistory } from 'react-router-dom'
+import { Avatar, Dropdown, Menu } from 'antd'
+import { UserOutlined, ExportOutlined } from '@ant-design/icons'
+import style from './style.module.less'
 
 const UserMenu = () => {
+  const history = useHistory()
+
+  const handleMenu = ({ key }) => {
+    console.log(key)
+    if (key === 'exit') {
+      history.push('/login')
+    }
+  }
+
+  const menu = (
+    <Menu onClick={handleMenu}>
+      <Menu.Item key='userInfo' icon={<UserOutlined />}>
+        个人中心
+      </Menu.Item>
+      <Menu.Item key='exit' icon={<ExportOutlined />}>
+        退出登录
+      </Menu.Item>
+    </Menu>
+  )
+
   return (
-    <div>
-      <Avatar size={40} icon={<UserOutlined />} />
+    <div className={style.userMenu}>
+      <Dropdown overlay={menu}>
+        <div className={style['wrapper']} style={{ display: 'flex' }}>
+          <Avatar size={36} icon={<UserOutlined />} />
+          <span>管理员</span>
+        </div>
+      </Dropdown>
     </div>
   )
 }
--- src/views/login/index.jsx
+++ src/views/login/index.jsx
@@ -1,46 +1,59 @@
 import React, { useState } from 'react'
-import { Button, List, Input } from 'antd'
-import { atom, useRecoilValue, useSetRecoilState } from 'recoil'
+import { Button, Input, Checkbox, message } from 'antd'
+import { useHistory } from 'react-router-dom'
 import style from './style.module.less'
 
-const todoListState = atom({
-  key: 'todoListState',
-  default: [
-    {
-      name: 123213123,
-    },
-  ],
-})
-
 const Login = () => {
-  const todoList = useRecoilValue(todoListState)
-  const setTodoList = useSetRecoilState(todoListState)
-  const [inputValue, setInputValue] = useState('')
-  const handleAdd = () => {
-    setTodoList((oldList) => [
-      ...oldList,
-      {
-        name: inputValue,
-      },
-    ])
-    setInputValue('')
+  const history = useHistory()
+
+  const [isLoginSucces, setIsLoginSucces] = useState(false)
+
+  const handleLogin = () => {
+    setIsLoginSucces(true)
+    setTimeout(() => {
+      const isSuccess = Math.random().toFixed(1) > 0.5
+      console.log(isSuccess)
+      if (isSuccess) {
+        history.push('/')
+      } else {
+        message.error('登录失败，请稍候再试！')
+        setIsLoginSucces(false)
+      }
+    }, [2000])
   }
+
   return (
-    <div>
-      <h1>login</h1>
-      <div className={style.content}>
-        <List
-          dataSource={todoList}
-          renderItem={(item) => {
-            return <List.Item>{item.name}</List.Item>
-          }}
-        />
-        <div style={{ display: 'flex' }}>
-          <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-          <Button type='primary' onClick={handleAdd}>
-            add
-          </Button>
-        </div>
+    <div className={style['login-container']}>
+      <div className={style['login-wrapper']}>
+        <form>
+          <div className={style['login-item']}>
+            <h1 className={style.title}>LOGIN</h1>
+          </div>
+          <div className={style['login-item']}>
+            <div className={style['input-wrap']}>
+              <label className={style['login-label']} htmlFor='username'>
+                账号
+              </label>
+              <Input id='username' placeholder='请输入账号' className={style['login-input']} />
+            </div>
+          </div>
+          <div className={style['login-item']}>
+            <div className={style['input-wrap']}>
+              <label className={style['login-label']} htmlFor='password'>
+                密码
+              </label>
+              <Input type='password' id='password' placeholder='请输入密码' className={style['login-input']} />
+            </div>
+          </div>
+          <div className={style['login-item']}>
+            <Checkbox>记住密码</Checkbox>
+          </div>
+          <div className={style['login-item']}>
+            <Button size='large' loading={isLoginSucces} block type='primary' onClick={handleLogin}>
+              登录
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   )
